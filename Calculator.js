e6e09d3b9c7d888821d2a8544835f4fb6fdfbf31
ch4mi2 <chamithu4@gmail.com>
2025-07-26 00:20:40 +0530

    feature b

--- Calculator.js
+++ Calculator.js
@@ -25,7 +25,6 @@ class Calculator {
     }
 }
 
-
 function validateNumbers(a, b) {
     if (typeof a !== 'number' || typeof b !== 'number') {
         throw new Error('Both arguments must be numbers');
