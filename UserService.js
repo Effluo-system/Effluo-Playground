ab45f2e888617c2a4fa14925490aa9ff9e0d8b85
Chamithu Gunawardana <95626480+ch4mi2@users.noreply.github.com>
2025-07-26 00:06:36 +0530

    Update UserService.js

--- UserService.js
+++ UserService.js
@@ -1,39 +1,23 @@
 class UserService {
     constructor() {
-        this.apiUrl = 'https://api.example.com/v2';  
+        this.apiUrl = 'https://api.example.com/v1';
         this.timeout = 5000;
-        this.authToken = null;  
-    }
-
-    setAuthToken(token) {
-        this.authToken = token;
     }
 
     async getUser(userId) {
-        const headers = {};
-        if (this.authToken) {
-            headers['Authorization'] = `Bearer ${this.authToken}`;
-        }
-        
         const response = await fetch(`${this.apiUrl}/users/${userId}`, {
             method: 'GET',
-            headers: headers,
             timeout: this.timeout
         });
         return response.json();
     }
 
     async updateUser(userId, userData) {
-        const headers = {
-            'Content-Type': 'application/json'
-        };
-        if (this.authToken) {
-            headers['Authorization'] = `Bearer ${this.authToken}`;
-        }
-
         const response = await fetch(`${this.apiUrl}/users/${userId}`, {
             method: 'PUT',
-            headers: headers,
+            headers: {
+                'Content-Type': 'application/json'
+            },
             body: JSON.stringify(userData),
             timeout: this.timeout
         });
